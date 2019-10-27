48509be2ae999741436ad3dd5963493bcb563f32
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-27 23:58:59 +0530

    Initializing Api calls

--- Backend/routes/dialogFlow.js
+++ Backend/routes/dialogFlow.js
@@ -5,3 +5,14 @@ const router = express.Router();
 
 router.post("/api/df_text_query", async (req, res) => {
     const { text, userID, parameters } = req.body;
+
+    const responses = await chatbot
+    .textQuery(text, userID, parameters)
+    .catch(err => {
+        console.log("ERROR:", err);
+        res.status(400).send("error");
+      });
+    
+    const result = responses[0].queryResult;
+    res.send(result);
+  });
