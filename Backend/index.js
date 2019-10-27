eef20f1aec16f4db6b12cc69ce4521ba2b6beafd
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-27 23:16:55 +0530

    Requiring Routes

--- Backend/index.js
+++ Backend/index.js
@@ -5,3 +5,7 @@ const config = require("./config/keys");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Routes
+const dialogFlowRoutes = require("./routes/dialogFlow");
+const fulfillmentRoutes = require("./routes/fulfillment");
