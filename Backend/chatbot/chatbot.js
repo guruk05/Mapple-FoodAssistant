c014d70c71e078c9e5320a448424ee6cc1c6d0f2
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-28 23:51:52 +0530

    Getting response from dialogflow using async await

--- Backend/chatbot/chatbot.js
+++ Backend/chatbot/chatbot.js
@@ -12,3 +12,26 @@ const credentials = {
   };
 
   const sessionClient = new dialogFlow.SessionsClient({ projectID, credentials });
+
+  module.exports = {
+    textQuery: async function(text, userID, parameters = {}) {
+      let sessionPath = sessionClient.sessionPath(projectID, sessionID + userID);
+  
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: text,
+            languageCode: languageCode
+          }
+        },
+        queryParams: {
+          payload: {
+            data: parameters
+          }
+        }
+      };
+      let responses = await sessionClient.detectIntent(request);
+      responses = await this.handleAction(responses);
+      return responses;
+    },
