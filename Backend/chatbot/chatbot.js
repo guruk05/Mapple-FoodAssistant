7aca9dc38fdbf834f0625b1d3569ccbc62aa93e6
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-28 23:48:54 +0530

    Assinging IDs to variables

--- Backend/chatbot/chatbot.js
+++ Backend/chatbot/chatbot.js
@@ -1,3 +1,7 @@
 const dialogFlow = require("dialogflow");
 const structjson = require("./structjson");
 const config = require("../config/keys");
+
+const projectID = config.googleProjectID;
+const sessionID = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
