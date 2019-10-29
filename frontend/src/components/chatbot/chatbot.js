58ecf7fae482ed7fdc8186bec7d45bd41998b243
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-29 11:33:52 +0530

    importing node modules in component

---
+++ frontend/src/components/chatbot/chatbot.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { v4 as uuid } from "uuid";
+import Message from "./Message";
+import Card from "./Card";
+import messageSound from './../../assets/open-ended.mp3';
