5cb141e92d74f95211d67cdead8aa77053728d5e
SudheerReddySingam <54614069+SudheerReddySingam@users.noreply.github.com>
2019-10-29 15:04:28 +0530

    ..

--- Backend/models/dish schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const dishSchema = new Schema({
-    dish: String,
-    link: String
-});
-
-mongoose.model('dish', dishSchema);
---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// Add this file to gitignore
+
+module.exports = {
+    google:{
+        clientID:'912172287810-tt4n4120c0v0i2r98bhbep9mi21utajk.apps.googleusercontent.com',
+        clientSecret:'pJHhnO2FpKu8yPYQp-j2lcqi'   
+    },
+    mongodb:{
+        dbURI:'mongodb+srv://guru:SIDejD1WzBnhpsC8@cluster0-4sldr.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session:{
+        cookieKey:'IamtheKingofthiswholeWorld'
+        }
+};
