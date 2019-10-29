170f5d09dd4b5270a65c3a26bb61272e5a67f5ae
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-29 11:54:56 +0530

    creating chatbot component

--- frontend/src/components/chatbot/chatbot.js
+++ frontend/src/components/chatbot/chatbot.js
@@ -7,3 +7,59 @@ import Card from "./Card";
 import messageSound from './../../assets/open-ended.mp3';
 
 const cookies = new Cookies();
+
+
+class Chatbot extends Component {
+    // Creating react references to elements
+    messagesEnd;
+    chatInput;
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        messages: [],
+        showBot: false,
+        welcomeSent: false,
+        botName: 'Chatbot'
+      };
+      
+      this.sound = new Audio(messageSound);
+      
+      //Setting the cookie using uuid
+      if (!cookies.get("userID")) {
+        cookies.set("userID", uuid(), { path: "/" });
+      }
+  
+      //Binding event listeners
+      this.toggleBot = this.toggleBot.bind(this);
+      this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+    }
+  
+    resolveAfterXSeconds(time) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(time);
+        }, time * 1000);
+      });
+    }
+  
+    async componentDidMount() {
+      if(!this.state.welcomeSent) {
+        await this.resolveAfterXSeconds(1.2);
+        this.df_event_query("WELCOME_TO_SITE");
+        this.setState({ welcomeSent: true, showBot: true });
+      }
+    }
+  
+    // Scroll to latest message on updation of state
+    componentDidUpdate() {
+      if(this.state.showBot) {
+        this.messagesEnd.scrollIntoView({ behaviour: "smooth" });
+      }
+      if (this.chatInput) {
+        this.chatInput.focus();
+      }
+    }
+
+    
+}
