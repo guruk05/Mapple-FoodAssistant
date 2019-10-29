bcf578c72eb46fcfbfc4fef89176d7f50c4ab2be
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-29 11:53:46 +0530

    initializing cookies

--- frontend/src/components/chatbot/chatbot.js
+++ frontend/src/components/chatbot/chatbot.js
@@ -5,3 +5,5 @@ import { v4 as uuid } from "uuid";
 import Message from "./Message";
 import Card from "./Card";
 import messageSound from './../../assets/open-ended.mp3';
+
+const cookies = new Cookies();
