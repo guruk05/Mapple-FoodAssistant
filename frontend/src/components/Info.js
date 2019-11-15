0c38e70a40b7f8bedda9da66596457a4d4f69323
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-15 10:25:03 +0530

    Created a card component

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -2,6 +2,24 @@ import React from 'react';
 import { Card , Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Info = () => {
-    
-}
+const Info = () => (
+    	<Card style={{ width: '20rem' , height: "31rem"}}>
+  <Card.Img  style={{ width: '20rem'}} variant="top" src="https://medcitynews.com/wp-content/uploads/2017/11/GettyImages-840949292-600x450.jpg"/>
+  <Card.Body>
+    <Card.Title>Food Assistance</Card.Title>
+    <Card.Text >
+      Hi, I am Mapple .
+      <br></br>
+      U can ask me about Foods and Restaurants
+      <br></br>
+      I am here to help you.
+    </Card.Text>
+    <Card.Text>
+      To know how i am built 
+    </Card.Text>
+    <Button className = "button" variant="secondary"><a className = "apply" href = "https://github.com/guruk05/Mapple-FoodAssistant">Click here</a></Button>
+  </Card.Body>
+</Card>
+);
+
+
