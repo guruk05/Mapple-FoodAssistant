499627d4748347eef03f89d751c1c878f885e26a
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-15 10:09:50 +0530

    Add comments in chatbit.js file

--- Backend/chatbot/chatbot.js
+++ Backend/chatbot/chatbot.js
@@ -2,6 +2,8 @@ const dialogFlow = require("dialogflow");
 const structjson = require("./structjson");
 const config = require("../config/keys");
 
+// Integrating DialogFlow 
+
 const projectID = config.googleProjectID;
 const sessionID = config.dialogFlowSessionID;
 const languageCode = config.dialogFlowSessionLanguageCode;
